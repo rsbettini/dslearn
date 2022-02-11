fb354fda9f07bfe74e2a11db5e48360992dcaf2f
rsbettini <robson.bettini@hotmail.com>
2022-02-10 22:22:29 -0300

    Custom content for current user

--- backend/src/main/java/com/devsuperior/dslearnbds/repositories/NotificationRepository.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/repositories/NotificationRepository.java
@@ -1,9 +1,14 @@
 package com.devsuperior.dslearnbds.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devsuperior.dslearnbds.entites.Notification;
+import com.devsuperior.dslearnbds.entites.User;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long>{
 
+	Page<Notification> findByUser(User user, Pageable pageable);
+	
 }
